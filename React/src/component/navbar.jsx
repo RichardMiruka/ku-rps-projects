f2ea500ad2ae6b897ae348a9622e98f6b18301e0
RichardMiruka <richardonsare15@gmail.com>
2024-06-11 15:51:40 +0300

    finished the basics of creating the application

--- React/src/component/navbar.jsx
+++ React/src/component/navbar.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import { link } from "react-router-dom";
 
-const Navbar = () => (
-    <nav>
-        <h2 className="logo">
-            <a className="logo-link" href="#">This is a Blog Application</a>
-        </h2>
-        <ul className="nav-menu">
-            <li><a className="nav-menu-link" href="#">Home</a></li>
-            <li><a className="nav-menu-link" href="#">Actors</a></li>
-            <li><a className="nav-menu-link" href="#">Best Films</a></li>
-        </ul>
-    </nav>
-);
+export default function Navbar(props) {
+    return (
+        <nav>
+            <h2 className="logo">
+                <Link className="logo-link" to="/">
+                {props.title}
+                </Link>{""}
+            </h2>
+            <ul className="nav-menu">
+                <li>
+                    <Link className="nav-menu_link" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-menu_link" to="/actors">
+                        Best Actors
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-menu_link" to="/films">
+                        Best Films
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
 
-export default Navbar;
+// import React from "react";
+
+// const Navbar = () => (
+//     <nav>
+//         <h2 className="logo">
+//             <a className="logo-link" href="#">This is a Blog Application</a>
+//         </h2>
+//         <ul className="nav-menu">
+//             <li><a className="nav-menu-link" href="#">Home</a></li>
+//             <li><a className="nav-menu-link" href="#">Actors</a></li>
+//             <li><a className="nav-menu-link" href="#">Best Films</a></li>
+//         </ul>
+//     </nav>
+// );
+
+// export default Navbar;
